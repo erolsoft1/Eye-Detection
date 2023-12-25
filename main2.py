2562cf9d888244a573a7409628b379c286354dca
erolsoft1 <145672766+erolsoft1@users.noreply.github.com>
2023-12-25 11:48:01 -0500

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy
+
+vid = cv2.VideoCapture(0)
+
+yuz_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+while True:
+    ret, frame = vid.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    yuzler = yuz_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for x, y, w, h in yuzler:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (85, 255, 0), 3)
+
+    cv2.imshow("title", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+vid.release()
+cv2.destroy.AllWindows()
